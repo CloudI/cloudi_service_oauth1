7d68f6c71ed471b8b2bf204fa0a549185ce71833
Michael Truog <mjtruog@gmail.com>
2017-03-28 21:34:31 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule CloudIServiceOauth1 do
   use Mix.Project
 
   def project do
     [app: :cloudi_service_oauth1,
-     version: "1.6.0",
+     version: "1.7.0",
      language: :erlang,
      description: description(),
      package: package(),
@@ -12,10 +15,10 @@ defmodule CloudIServiceOauth1 do
 
   defp deps do
     [# TODO, add cloudi_service_db_pgsql dependencies
-     {:cloudi_service_db_pgsql, "~> 1.6.0"},
+     {:cloudi_service_db_pgsql, "~> 1.7.0"},
      {:cowlib, "~> 1.0.0"},
      {:jsx, "~> 2.8.0"},
-     {:cloudi_core, "~> 1.6.0"}]
+     {:cloudi_core, "~> 1.7.0"}]
   end
 
   defp description do
